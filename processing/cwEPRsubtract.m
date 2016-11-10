152dd4cb4083671dad610b73a43f2c0121ad8388
Jara Popp <j.popp@gmx.ch>
2016-11-10 15:55:23 +0100

    Add function to subtract data from dataset

---
+++ processing/cwEPRsubtract.m
@@ -0,0 +1,33 @@
+function dataset = cwEPRsubtract(dataset,tosubtract)
+% CWEPRSUBTRACT subtract any vector with same length as dataset.data 
+% from a cw-EPR spectrum and writes history
+%
+% Usage
+%   dataset = cwEPRsubtract(dataset,vector)
+%
+%   dataset    - struct
+%                structure containing data and additional fields
+%
+%
+%   tosubtract - vector 
+%                any vector with same length as dataset.data
+%                            
+
+% Copyright (c) 2016, Till Biskup, Jara Popp, Deborah Meyer
+% 2016-11-10
+
+if ~isequal(size(tosubtract),size(dataset.data))
+    disp(['(EE) ' 'vector length not equal to data length']);
+    return
+end
+
+dataset.data = dataset.data-tosubtract;
+
+% Write history
+history = cwEPRhistoryCreate();
+history.kind = 'Subtraction';
+history.purpose = 'Subtraction of given vector from data';
+history.parameters = {tosubtract};
+dataset.history{end+1} = history;
+
+end
