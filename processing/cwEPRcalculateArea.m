04fa18d006159e567e32aac2d26231078aeabd13
Till Biskup <till@till-biskup.de>
2020-10-05 15:14:15 +0200

    Area calculation returns full dataset in case of original 2D dataset

--- processing/cwEPRcalculateArea.m
+++ processing/cwEPRcalculateArea.m
@@ -1,17 +1,24 @@
-function area = cwEPRcalculateArea(dataset)
+function return_value = cwEPRcalculateArea(dataset)
 % CWEPRCALCULATEAREA calculates area under a given curve
 %
 % Usage
 %   dataset = cwEPRintregrate(dataset)
+%   new_dataset = cwEPRintregrate(dataset)
 %
-%   dataset - struct
-%             structure containing data and additional fields
+%   dataset  - struct
+%              structure containing data and additional fields
 %
-%   area    - scalar/vector
-%             area under the curve(s)
+%   area     - scalar/vector
+%              area under the curve(s)
 %
-%             For 2D datasets, summing is performed along the first
-%             dimension
+%              For 2D datasets, summing is performed along the first
+%              dimension
+%
+%   dataset2 - struct
+%              structure containing data and additional fields
+%
+%              Returned in case of 2D datasets, with axes adjusted
+%              accordingly.
 %
 % WARNING: For reasonably correct results regarding cwEPR spectra, those
 % spectra should be baseline-corrected, afterwards integrated, again
@@ -28,6 +35,21 @@ function area = cwEPRcalculateArea(dataset)
 % Copyright (c) 2020, Till Biskup
 % 2020-10-05
 
-area = sum(dataset.data, 1);
+if isvector(dataset.data)
+    return_value = sum(dataset.data);
+else
+    return_value = dataset;
+    return_value.data = sum(return_value.data, 1);
+    return_value.axes.data(1) = return_value.axes.data(2);
+    return_value.axes.data(2) = return_value.axes.data(3);
+    return_value.axes.data(2).measure = 'integrated intensity';
+    return_value.axes.data(3) = [];
+    
+    % Write history
+    history = cwEPRhistoryCreate();
+    history.kind = 'Area calculation';
+    history.purpose = 'Calculate area below curve';
+    return_value.history{end+1} = history;
+end
 
 end
